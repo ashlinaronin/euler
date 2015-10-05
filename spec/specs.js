afd23949ca4b158bdf618e1a6500864acfc4c9b1
Ashlin <ashlinaronin@gmail.com>
2015-10-05 13:37:05 -0700

    Write second test for Euler 1 and make it fail

--- spec/specs.js
+++ spec/specs.js
@@ -2,6 +2,10 @@ describe('multipleThreeFive', function() {
     it('lists out all natural numbers below 10 that are multiples of 3 or 5', function() {
       expect(multipleThreeFive(10)).to.eql([3,5,6,9]);
     });
+
+    it('lists out all natural numbers below 30 that are multiples of 3 or 5', function() {
+      expect(multipleThreeFive(10)).to.eql([3,5,6,9,10,12,15,18,20,21,24,25,27]);
+    });
 });
 
 
