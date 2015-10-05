c0fd87ade8a2636ea96ded8c9741aa24c1593088
Ashlin <ashlinaronin@gmail.com>
2015-10-05 13:43:35 -0700

    Pass third spec, submit solution

--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,13 @@
 describe('sumMultiples', function() {
-    it('lists out all natural numbers below 10 that are multiples of 3 or 5', function() {
+    it('sums all natural numbers below 10 that are multiples of 3 or 5', function() {
       expect(sumMultiples(10)).to.equal(23);
     });
 
-    it('lists out all natural numbers below 30 that are multiples of 3 or 5', function() {
+    it('sums all natural numbers below 30 that are multiples of 3 or 5', function() {
       expect(sumMultiples(30)).to.equal(195);
+    });
+
+    it('sums all natural numbers below 1000 that are multiples of 3 or 5', function() {
+      expect(sumMultiples(1000)).to.equal(233168);
     });
 });
