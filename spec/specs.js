9fe7ecad1a5689169fdeedabaeb9112cb27caad1
Ashlin <ashlinaronin@gmail.com>
2015-10-05 13:48:19 -0700

    Write first test for fibonacci and fail

--- spec/specs.js
+++ spec/specs.js
@@ -1,13 +1,19 @@
-describe('sumMultiples', function() {
-    it('sums all natural numbers below 10 that are multiples of 3 or 5', function() {
-      expect(sumMultiples(10)).to.equal(23);
-    });
+describe('1. sumMultiples', function() {
+  it('sums all natural numbers below 10 that are multiples of 3 or 5', function() {
+    expect(sumMultiples(10)).to.equal(23);
+  });
 
-    it('sums all natural numbers below 30 that are multiples of 3 or 5', function() {
-      expect(sumMultiples(30)).to.equal(195);
-    });
+  it('sums all natural numbers below 30 that are multiples of 3 or 5', function() {
+    expect(sumMultiples(30)).to.equal(195);
+  });
 
-    it('sums all natural numbers below 1000 that are multiples of 3 or 5', function() {
-      expect(sumMultiples(1000)).to.equal(233168);
-    });
+  it('sums all natural numbers below 1000 that are multiples of 3 or 5', function() {
+    expect(sumMultiples(1000)).to.equal(233168);
+  });
+});
+
+describe('2. fibonacci', function() {
+  it('generates the first 10 terms in the fibonacci sequence', function() {
+    expect(fibonacci(10)).to.eql([1, 2, 3, 5, 8, 13, 21, 34, 55, 89]);
+  });
 });
