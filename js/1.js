0a987423b414fa459032443fa2e9c685821ab4dd
Ashlin <ashlinaronin@gmail.com>
2015-10-05 13:39:23 -0700

    Pass second test for Euler 1

--- js/1.js
+++ js/1.js
@@ -4,5 +4,13 @@
 ** Find the sum of all the multiples of 3 or 5 below 1000. */
 
 var multipleThreeFive = function(number) {
-  return [3,5,6,9];
+  var multiples = [];
+  // Iterate through all numbers less than input
+  for (var i = 1; i < number; i++) {
+    if ((i % 3 === 0) || (i % 5 === 0)) {
+      multiples.push(i);
+    }
+  }
+
+  return multiples;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -4,34 +4,6 @@ describe('multipleThreeFive', function() {
     });
 
     it('lists out all natural numbers below 30 that are multiples of 3 or 5', function() {
-      expect(multipleThreeFive(10)).to.eql([3,5,6,9,10,12,15,18,20,21,24,25,27]);
+      expect(multipleThreeFive(30)).to.eql([3,5,6,9,10,12,15,18,20,21,24,25,27]);
     });
 });
-
-
-//
-// describe('columnCount', function() {
-//     it("returns the square root for an input with a whole number square root", function() {
-//         // 64 character input -> 8 columns
-//         var inputString = "don't compare yourself to others, compare yourself person you were yesterday";
-//         var flattenedString = format(inputString);
-//
-//         expect(columnCount(flattenedString)).to.equal(8);
-//     });
-//
-//     it("returns the square root for an input with a smaller whole number square root", function() {
-//         // 16 character input -> 4 columns
-//         var inputString = "Portlands NachoWk!";
-//         var flattenedString = format(inputString);
-//
-//         expect(columnCount(flattenedString)).to.equal(4);
-//     });
-//
-//     it("handles a non-whole-number whole root case", function() {
-//         // 69 character input -> 9 columns
-//         var inputString = "don't compare yourself to others, compare yourself to the person you were yesterday";
-//         var flattenedString = format(inputString);
-//
-//         expect(columnCount(flattenedString)).to.equal(9);
-//     });
-// });
